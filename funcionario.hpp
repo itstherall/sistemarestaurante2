b81b736b3738ffd36c49da7be9c83f1abb0f4552
teugoes <teugoes@hotmail.com>
2019-11-17 16:53:30 -0300

    doxygen

--- funcionario.hpp
+++ funcionario.hpp
@@ -4,14 +4,15 @@
 #include <fstream>
 
 using namespace std;
-
+///Declaracao da classe funcionario
 class Funcionario{
 private:
+	///Atributos da classe funcionario
 	string nome;
 	string cpf;
 
 public:
-	
+	///Metodos da classe funcionario
 	Funcionario(string nome, string cpf);
 	string Rnome();
 	string Rcpf();
