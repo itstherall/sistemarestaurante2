f9da7fe02536ccacf1ed5c4cf0554589443e378b
teugoes <teugoes@hotmail.com>
2019-11-17 16:45:12 -0300

    doxygen

--- funcionario.cpp
+++ funcionario.cpp
@@ -6,11 +6,13 @@
 
 using namespace std;
 
+///Construtores da classe funcionarios
 Funcionario::Funcionario(string nome, string cpf){
 		this->nome = nome;
 		this->cpf = cpf;
 	}
-
+///Metodo returnNome
 string Funcionario::Rnome() { return nome; }
 
+///Metodo returnCpf
 string Funcionario::Rcpf() { return cpf; }
